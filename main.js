407989b6dd192dbe53c7ec3aa9218cb755cec96d
David Castillo <seth.castillo4@gmail.com>
2021-10-20 21:47:00 -0500

    Finished Project

---
+++ main.js
@@ -0,0 +1,37 @@
+let sort = () => {
+ /*Below the code is filtering out anything thats not a letter*/
+ let temp = document.getElementById('userInput').value.split('')
+ let listToSort=[]
+ temp.forEach(e => {
+  if (/[a-zA-Z]+$/.test(e)) {
+   listToSort.push(e)
+  }
+ });
+
+ /*Below is couting each occurence of the letters*/
+
+ let word = listToSort.join('')
+ let Count = {}
+ 
+ for (let i = 0; i < word.length; i++) {
+   let letter = word[i]
+   let vara = Count[letter]
+   if (!vara) {
+     Count[letter] = 1
+   }
+   else {
+     Count[letter] = vara + 1
+   }
+  }
+ console.log(Count)
+
+  /*Lastly we are converting an object to observable date that we can display on the DOM*/
+
+ let str = JSON.stringify(Count).split(',').join('  ')
+ let div = document.getElementById('sorted')
+ div.append(str)
+document.body.appendChild(div)
+}
+
+
+
